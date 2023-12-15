cd70d5d203abf08668591aed9afca6aa0ddb0598
Utkarsh Padia <utkarshpadia@gmail.com>
2023-12-15 23:52:27 +0530

    project complete

--- main.py
+++ main.py
@@ -12,4 +12,12 @@ imdb_link = driver.find_element_by_xpath('/html/body/div[5]/div/div[10]/div[1]/d
 imdb_link.click()
 time.sleep(7)
 
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
+
+driver.find_element_by_tag_name('body').send_keys(Keys.PAGE_UP)
+driver.save_screenshot("screenshot.png")
+
+poster = driver.find_element_by_xpath('/html/body/div[2]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[2]/ul/li[50]/div[1]/div/div/div[1]/div[1]/div/a/div')
+poster.screenshot('screenshot2.png')
+driver.quit()
